5c8e6cfe7d212f3a0a8591a70ebbf7c065911eb9
= <solrunasta@hotmail.com>
2020-02-06 16:20:04 +0000

    finished 12

--- MakeBelieveJS_solla.js
+++ MakeBelieveJS_solla.js
@@ -31,7 +31,7 @@
   MakeBelieveElement.prototype.append = function(arg) {
     for (var i = 0; i < this.nodes.length; i++) {
       if (typeof arg == 'object') {
-        arg = '<' + arg.tagName + '>' + arg.data + '</' + arg.tagName + '>'
+        arg = arg.parentNode.outerHTML
       }
       this.nodes[i].insertAdjacentHTML('afterend', arg)
     }
@@ -40,7 +40,6 @@
   // section 10
 
   MakeBelieveElement.prototype.prepend = function(arg) {
-    console.log(arg.parentNode);
     for (var i = 0; i < this.nodes.length; i++) {
       if (typeof arg == 'object') {
         arg = arg.parentNode.outerHTML
@@ -49,7 +48,9 @@
     }
   }
 
+
   // section 14
+
   MakeBelieveElement.prototype.toggleClass = function(arg) {
     for (var i = 0; i < this.nodes.length; i++) {
       if (this.nodes[i].className == arg) {
@@ -61,6 +62,45 @@
     }
   }
 
+  // section 12
+
+  function ajax(config) {
+    // setting default value
+    if (config.method == '') {
+      config.method = 'GET';
+    }
+
+    // create xhttp request and open
+    var xhttp = new XMLHttpRequest();
+    xhttp.open(config.method, config.url);
+
+    // setting req headers
+    for (var i = 0; i < config.headers.length; i++) {
+      xhttp.setRequestHeader(
+        Object.keys(config.headers[i]), config.headers[i][Object.keys(config.headers[i])]
+      );
+    }
+
+    // setting timeout
+    xhttp.timeout = config.timeout * 1000;
+
+    // how did it go?
+    xhttp.onload = function () {
+      config.beforeSend()
+      if (xhttp.readyState == xhttp.DONE) {
+        if ( Math.floor(xhttp.status/100) == 2 ) {
+          config.success()
+        }
+        else (
+          config.fail()
+        )
+      }
+    }
+
+      // send request
+      xhttp.send(config.data)
+  }
+
   function query(cssSelector) {
     // get items
     var items = document.querySelectorAll(cssSelector);
@@ -68,17 +108,27 @@
   }
 
   globalObj.__ = query;
+  globalObj.__.ajax = ajax;
 })(window);
 
 
 var classThird = __('.item-1');
 
-classThird.prepend('<p>Testery</p>');
-__('p').prepend(
-  document.createElement('p')
-  .appendChild(
-    document.createTextNode('Im a text node')
-  )
-)
+__.ajax({
+  url: 'https://serene-island-81305.herokuapp.com/api/200',
+  method: 'GET',
+  timeout: 10,
+  data: {},
+  headers: [],
+  success: function (resp) {
+    console.log('SUCCESSS')
+  },
+  fail: function (error) {
+    console.log('FAIL')
+  },
+  beforeSend: function (xhr) {
+    console.log('BEFORESEND')
+  }
+});
 
-__('p').toggleClass('SomeClass')
+__
